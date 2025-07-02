52e8bdf3ba6662ee53bd97c528ec255ab5c938dc
IlyassSama <98023865+IlyassSama@users.noreply.github.com>
2025-07-02 11:41:44 +0100

    Create Buildings.lua

---
+++ Buildings.lua
@@ -0,0 +1,12 @@
+local Buildings = {
+	["Podium"] = {
+		Type = "Furniture",
+		Price = 50,
+		DecorationValue = 2,
+		PlacementSize = Vector2.new(1, 1),
+		Model = "Podium", 
+		Rarity = "Common",
+	},
+}
+
+return Buildings
